f3eb9d89bc7313bd97078e0e1885fa1ef4204ece
zhicheng <zchenggu@gmail.com>
2016-04-09 23:51:44 -0500

    1

--- ml/value_function.py
+++ ml/value_function.py
@@ -1,9 +1,14 @@
 
-def corner_different_value_function(board):
+def corner_different(board):
     return len(board.find_user_corners(board.user2)) - len(board.find_user_corners(board.user1))
+
+
+def oppenent_corners(board):
+    return -len(board.find_user_corners(board.user1)) * 1000 + len(board.find_user_corners(board.user2))
 
 
 class ValueFunction:
     # corner_difference = corner_different_value_function
     def __init__(self):
-        self.corener_difference = corner_different_value_function
+        self.corener_difference = corner_different
+        self.oppenent_corners = oppenent_corners
--- ui/board_ui.py
+++ ui/board_ui.py
@@ -81,6 +81,6 @@ class BoardUI(Frame):
         if len(self.board.user_possible_puts(self.board.user2)) == 0:
             print 'User 2 out of move'
             return
-        move = one_level_search(self.board, ValueFunction().corener_difference)
+        move = one_level_search(self.board, ValueFunction().oppenent_corners)
         self.draw_block_on_point(move[0], move[1], 'purple', True)
         self.board.user_put_block_on_point(self.board.user2, move[0], move[1])
