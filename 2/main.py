080963c89c32519606d844267225e7fad702aad2
Zhicheng <zchenggu@gmail.com>
2016-04-11 22:41:06 -0500

    2

---
+++ 2/main.py
@@ -0,0 +1,9 @@
+from model import *
+import copy
+
+init_block_pool()
+print len(block_pool)
+
+for block in block_pool:
+    print len(block)
+
---
+++ 2/model/__init__.py
@@ -0,0 +1,2 @@
+from block import block_pool, init_block_pool
+from board import init_board
