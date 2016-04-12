4ec9fde23cf3a53dc3c0663f6fce0e380ed9fa1e
zhicheng <zchenggu@gmail.com>
2016-04-12 01:13:45 -0500

    delete 1

--- 1/ml/depth_search.py
+++
@@ -1,7 +0,0 @@
-import time
-
-TIME_OUT = 5.0
-
-
-def depth_search(board, value_function):
-    start_time = time.time()
--- 1/ui/__init__.py
+++
@@ -1,2 +0,0 @@
-from board_ui import BoardUI
-from control_ui import UIController
