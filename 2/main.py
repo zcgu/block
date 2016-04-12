1672a2712c3a62b5d8d9a1df254897667190b420
Zhicheng <zchenggu@gmail.com>
2016-04-11 23:37:24 -0500

    2

--- 2/main.py
+++
@@ -1,9 +0,0 @@
-from model import *
-import copy
-
-init_block_pool()
-print len(block_pool)
-
-for block in block_pool:
-    print len(block)
-
--- 2/model/__init__.py
+++
@@ -1,2 +0,0 @@
-from block import block_pool, init_block_pool
-from board import init_board
