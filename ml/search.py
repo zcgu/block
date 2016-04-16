af9f048ac6b6d04a440ed9d12d9e0310388dca19
zhicheng <zchenggu@gmail.com>
2016-04-16 01:37:25 -0500

    2 finish

--- ml/search.py
+++ ml/search.py
@@ -2,27 +2,45 @@ from model import *
 import copy
 
 
-def one_level_search(board, user, value_func, return_list=False):
-    que = []
+def one_level_search(board, user, value_func, require_score=False):
+    moves, scores = init(board, user)
 
-    for move in possible_puts(board, user):
-        board2 = copy.deepcopy(board)
-        put_block(board2, user, move[0], move[1], move[2])
-        que.append([board2, move])
+    for move in moves:
+        board2 = perform(board, user, move)
+        scores[moves.index(move)] = value_func(board2, user)
 
-    que = sorted(que, key=lambda state: value_func(state[0], user), reverse=True)
+    if require_score:
+        return max(scores)
 
-    if return_list:
-        return que
-    else:
-        return que[0][1]
+    return result(moves, scores)
 
 
 def two_level_search(board, user, value_func):
-    que1 = one_level_search(board, user, value_func, True)
-    que1 = que1[:min(10, len(que1))]
-    for state in que1:
-        que2 = one_level_search(board, user1 + user2 - user, value_func, True)
-        state.append(value_func(que2[0][0], user1 + user2 - user))
-    que1 = sorted(que1, key=lambda states: states[2])
-    return que1[0][1]
+    moves, scores = init(board, user)
+
+    for move in moves:
+        board2 = perform(board, user, move)
+        scores[moves.index(move)] = -one_level_search(board2, other(user), value_func, True)
+
+    return result(moves, scores)
+
+
+def init(board, user):
+    moves = possible_puts(board, user)
+    scores = [float("-inf")] * len(moves)
+    return moves, scores
+
+
+def result(moves, scores):
+    max_score = max(scores)
+    return moves[scores.index(max_score)]
+
+
+def perform(board, user, move):
+    board2 = copy.deepcopy(board)
+    put_block(board2, user, move[0], move[1], move[2])
+    return board2
+
+
+def other(user):
+    return user1 + user2 - user
